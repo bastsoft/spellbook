33e65a91231b09ef6388b30317524225e15da7b7
p.laptev <p.laptev@i.krediska.ru>
2024-08-17 10:59:53 +0500

    fix lint

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ mv storybook-static docs
 cd docs
 echo " " > .nojekyll
 cd ..
+# git add .
+# git commit -m "deploy"
 # git push --set-upstream origin gh-pages -f
 # git checkout main
 # git branch -D gh-pages
